f8c7b3ea7f478efe3fc004ed70ac30eb5637adf7
BrunnoFBK <103584952+BrunnoFBK@users.noreply.github.com>
2024-09-05 08:13:53 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,78 @@
+export const perguntas = [
+    {
+        enunciado: "Você sabe o que é geometria?",
+        alternativas: [
+            {
+                texto: "Parte da matemática que ocupa somente triangulos",
+                afirmacao: [
+                    "Errado, embora a trigonometria se faça bastante presente"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Parte da matemática cujo objeto é o estudo do espaço e das figuras que podem ocupá-lo.",
+                afirmacao: [
+                    "Correto! Parabéns"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Onde é utilizada?",
+        alternativas: [
+            {
+                texto: "Na engenharia, a arquitetura, a astronomia, as pesquisas nas ciências exatas",
+                afirmacao: [
+                    "É utilizado nesses e em muitos outros casos"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "é pouco utilizada no dia a dia",
+                afirmacao: [
+                    "Errado, se faz presente em diversas areas"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "E na engenharia civil, como é usada?",
+        alternativas: [
+            {
+                texto: "São através de formas e cálculos geométricos, que profissionais ligados a estas áreas, esboçam as plantas das casas, prédios, ruas, praças.",
+                afirmacao: [
+                    "Correto, a geometria é amplamente utilizada na engenharia civil e graças a ela os profissionais podem executar as obras com muita precisão"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Não é utilizada",
+                afirmacao: [
+                    "Errado, a engenharia depende da geometria, pois sem ela se torna impossivel realizar obras com precisão" ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Você gostou de saber sobre?",
+        alternativas: [
+            {
+                texto: "Sim!",
+                afirmacao: [
+                    "Nós agradecemos! "
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não",
+                afirmacao: [
+                    "Agradecemos"
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+   
+];

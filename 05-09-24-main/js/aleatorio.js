2e7101fd722dce7c32b17cdd83a9bdfeb02c5769
BrunnoFBK <103584952+BrunnoFBK@users.noreply.github.com>
2024-09-12 07:49:01 -0300

    Add files via upload

---
+++ 05-09-24-main/js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["fulano" , "fulana"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
